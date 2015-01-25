56ef6ab02ac80fe847d96ac6bbb0f90ce82956a5
Wil Selwood <wselwood@gmail.com>
2015-01-25 11:04:02 +0000

    datatypes and args

--- HFSProcessor.c
+++ HFSProcessor.c
@@ -13,6 +13,61 @@ void check (int status, char * message, ...) {
   }
 }
 
+char* readHdfBytes(int32 sds_id, int* dimsizes) {
+  char* data = (char*) malloc(sizeof(char) * dimsizes[0] * dimsizes[1]);
+  int32 start[2] = {0, 0};
+
+  int status = SDreaddata(sds_id, start, NULL, dimsizes, data);
+  check(status, "Error reading chunk\n");
+  return data;
+}
+
+short* readHdfShort(int32 sds_id, int* dimsizes) {
+  short* data = (short*) malloc(sizeof(short) * dimsizes[0] * dimsizes[1]);
+  int32 start[2] = {0, 0};
+
+  int status = SDreaddata(sds_id, start, NULL, dimsizes, data);
+  check(status, "Error reading chunk\n");
+  return data;
+}
+
+int readAttrInt(int32 sds_id, int32 nt, int entry, int count, char * atrname, char * name) {
+
+  int status;
+  /* figure out the size of the attribute */
+  int32 dsize = DFKNTsize(nt);
+  check(dsize, "Error got invalid size for attribute %s in section %s\n", atrname, name);
+
+  if(nt == 21) {
+    char * tbuff = (char*) malloc(dsize * (count + 1));
+    if(tbuff == NULL) {
+      printf("Error could not allocate space for attribute\n");
+      return 1;
+    }
+
+    status = SDreadattr(sds_id, entry, tbuff);
+    check(status, "Error failed to read attribute %d from %d\n", atrname, name);
+    return *tbuff;
+  } else if(nt == 22) {
+
+    /* allocate space for the attribute */
+    short * tbuff = (short*) malloc(dsize * (count + 1));
+    if(tbuff == NULL) {
+      printf("Error could not allocate space for attribute\n");
+      return 1;
+    }
+
+    status = SDreadattr(sds_id, entry, tbuff);
+    check(status, "Error failed to read attribute %d from %d\n", atrname, name);
+    return *tbuff;
+  }
+  else {
+    printf("ERROR unknown datatype %d\n", nt);
+    exit(1);
+  }
+
+}
+
 int main (int argc, char** argv) {
 
   int32 nsds;
@@ -27,9 +82,17 @@ int main (int argc, char** argv) {
 
   int32 fillValue = -1;
 
+
+  if(argc != 3) {
+
+    printf("%d usage: hfsprocessor file part\n", argc);
+    return 1;
+  }
   //TODO: this should be passed in  some how.
-  char * filename = "/home/wselwood/data/real/MYD11A2.A2015009.h22v10.005.2015020130228.hdf";
-  char * requiredPart = "Clear_sky_days";
+  //char * filename = "/home/wselwood/data/real/MYD11A2.A2015009.h22v10.005.2015020130228.hdf";
+  char * filename = argv[1]; //"/home/wselwood/data/real/MYD13A3.A2014305.h22v10.005.2014349100449.hdf";
+  //char * requiredPart = "Clear_sky_days";
+  char * requiredPart = argv[2]; //"1 km monthly NDVI";
 
   for (int i = 0; i < 50; i++) {
     dimsizes[i] = 0;
@@ -51,27 +114,16 @@ int main (int argc, char** argv) {
 
 
     if (strcmp(name, requiredPart) == 0) {
+      int datatype = nt;
 
       for (int j = 0; j < nattrs; j++) {
         status = SDattrinfo(sds_id, j, atrname, &nt, &count);
         check(status, "Error getting attribute info for %s", name);
         //printf("attr %s type %d value %d\n", atrname, nt, count);
         if (strcmp("_FillValue", atrname) == 0) {
-          char *tbuff;
-          /* figure out the size of the attribute */
-          int32 dsize = DFKNTsize(nt);
-          check(dsize, "Error got invalid size for attribute %s in section %s\n", atrname, name);
-
-          /* allocate space for the attribute */
-          tbuff = (char*) malloc(dsize * (count + 1));
-          if(tbuff == NULL) {
-            printf("Error could not allocate space for attribute\n");
-            return 1;
-          }
-
-          status = SDreadattr(sds_id, j, tbuff);
-          fillValue = atoi(tbuff);
+          fillValue = readAttrInt(sds_id, nt, j, count, atrname, name);
         }
+
       }
 
       printf("%s x:%d y:%d fillValue:%d\n", name, dimsizes[0], dimsizes[1], fillValue);
@@ -79,30 +131,43 @@ int main (int argc, char** argv) {
         printf("ERROR fill value not found\n");
         return 1;
       }
-      char* data = (char*) malloc(sizeof(char) * dimsizes[0] * dimsizes[1]);
-      if(data == NULL) {
-        printf("ERROR could not allocate data\n");
+
+
+      if (datatype == 21) { // bytes
+        char * data = readHdfBytes(sds_id, dimsizes);
+        for (int y = 0; y < dimsizes[0]; y++) {
+          for (int x = 0; x < dimsizes[1]; x++) {
+            int value = data[(y*dimsizes[0])+x];
+            if (value != fillValue) {
+              printf("#");
+            } else {
+              printf(" ");
+            }
+          }
+          printf("\n");
+        }
+
+        free(data);
+      } else if (datatype == 22) {
+        short * data = readHdfShort(sds_id, dimsizes);
+        for (int y = 0; y < dimsizes[0]; y++) {
+          for (int x = 0; x < dimsizes[1]; x++) {
+            int value = data[(y*dimsizes[0])+x];
+            if (value != fillValue) {
+              printf("#");
+            } else {
+              printf(" ");
+            }
+          }
+          printf("\n");
+        }
+
+        free(data);
+      } else {
+        printf("ERROR unknown datatype %d\n", datatype);
         return 1;
       }
 
-      int32 start[2] = {0, 0};
-
-      status = SDreaddata(sds_id, start, NULL, dimsizes, data);
-      check(status, "Error reading chunk from %s\n", name);
-
-      for (int y = 0; y < dimsizes[0]; y++) {
-        for (int x = 0; x < dimsizes[1]; x++) {
-          char value = data[(y*dimsizes[0])+x];
-          if (value != fillValue) {
-            printf("#");
-          } else {
-            printf(" ");
-          }
-        }
-        printf("\n");
-      }
-
-      free(data);
     }
 
   }
